9fa5ff20874991ac29bb2798d9fcc820276a6bb3
James <jameswj111@gmail.com>
2024-12-04 17:00:30 -0500

    replaces ajax request with axios request, adds browserify

--- frontend/public/js/script.js
+++ frontend/public/js/script.js
@@ -1,24 +1,33 @@
+const axios = require('axios');
+
 $(()=> {
     console.log('hello from script.js')
 
+    axios.get('/api/data')
+         .then(response=> {
+          console.log(response.data)
+         })
+         .catch(error=> {
+          console.error(error)
+         })
     // get images on page load
-    $.ajax({
-        url: "/api/data",
-        method: "GET",
-        datatype: "json",
-        success: (data) => {
-            console.log(data)
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-            if (textStatus === 'error' && errorThrown === 'Connection refused') {
-              // Handle connection refused error
-              console.error('Connection refused!');
-            } else {
-              // Handle other AJAX errors
-            //   console.error('AJAX Error:', textStatus, errorThrown);
-            }
-        }
-    });
+    // $.ajax({
+    //     url: "/api/data",
+    //     method: "GET",
+    //     datatype: "json",
+    //     success: (data) => {
+    //         console.log(data)
+    //     },
+    //     error: function(jqXHR, textStatus, errorThrown) {
+    //         if (textStatus === 'error' && errorThrown === 'Connection refused') {
+    //           // Handle connection refused error
+    //           console.error('Connection refused!');
+    //         } else {
+    //           // Handle other AJAX errors
+    //         //   console.error('AJAX Error:', textStatus, errorThrown);
+    //         }
+    //     }
+    // });
 
     // async function fetchData() {
     //     try {
